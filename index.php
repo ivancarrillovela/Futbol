ba444c714d4b3e7127ce19b526bb533f2b28290d
Ivan <ivancarrillovela@gmail.com>
2025-10-28 18:15:31 +0100

    Implementación de la gestión de sesiones en index.php utilizando la clase GestorSesion

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-    $dir = __DIR__;
-    require_once $dir . "/templates/header.php";
-?>
 
-</body>
-</html>
+$dir = __DIR__;
+require_once $dir . "/utils/GestorSesion.php";
+
+GestorSesion::startSession(); 
+
+if (isset($_SESSION['last_team_viewed_id'])) {
+    $team_id = $_SESSION['last_team_viewed_id'];
+    header("Location: app/PartidosEquipo.php?id=" . $team_id);
+    exit;
+} else {
+    header("Location: app/Equipos.php");
+    exit;
+}
+
+?>
