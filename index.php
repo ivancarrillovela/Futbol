cd318b48ba79337f92290f08eda415e9a6c05146
Ivan <ivancarrillovela@gmail.com>
2025-10-28 14:14:21 +0100

    Creado header para todas las vistas y añadido al index

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    $dir = __DIR__;
+    require_once $dir . "/templates/header.php";
+?>
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,33 @@
+<?php
+    $dirHref = "/EjerciciosDWEB/Futbol";
+?>
+
+<head>
+    <meta charset="utf-8">
+    <title>Competición</title>
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="<?php echo $dirHref . "/assets/bootstrap/css/bootstrap.min.css" ?>">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="<?php echo $dirHref . "/index.php" ?>">Competición</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $dirHref . "/VISTAS/Equipos.php" ?>">Equipos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $dirHref . "/VISTAS/Partidos.php" ?>">Partidos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
